93beb549383a7a652a8c81481fb0b4559e780b5d
elmouak <developer14elmouak@gmail.com>
2024-06-09 19:02:08 +0200

    projets section

--- projects/js/projects.js
+++ projects/js/projects.js
@@ -1,15 +1,10 @@
 export let projects = 
 [
   {
-    title: "age calculation",
-    src: "age-calculation/index.html",
-    img: "project-2.PNG"
-  },
-  {
-    title: "form validation",
-    src: "clock/index.html",
-    img: "project-1.PNG"
-  },
+    title: "",
+    src: "error",
+    img: ""
+  }
 ];
 // git add .
 // git commit -m 'change'
